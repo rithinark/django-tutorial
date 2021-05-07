7edd588bd5716e6205b5ec9610b3ba9a412df155
rithin ritz <rithin316@gmail.com>
2021-05-07 15:41:09 +0530

    bootstrap form

--- tutorial/app/forms.py
+++ tutorial/app/forms.py
@@ -3,7 +3,6 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 class Register(UserCreationForm):
-    username = forms.CharField(max_length=40)
     class Meta:
         model = User
         fields = ['username' ,'password1', 'password2']
